29d8a1fc1ce0ae55936b13c116b1ae6bd78df48d
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-08-25 01:17:43 -0700

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://debashisbiswas.github.io/",
+  site: "https://dbiswas.com/",
   trailingSlash: "always",
   integrations: [
     tailwind({
