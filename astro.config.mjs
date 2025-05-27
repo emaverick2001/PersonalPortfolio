856bfc57d1c0b59a954ff0fee3957bb502fbb1f9
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2025-05-26 22:44:18 -0700

    fix large mobile image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
       config: { applyBaseStyles: false },
     }),
     sitemap(),
+    mdx(),
   ],
 })
